62622ed2dfa43306df03a2e77ed70ad984eb142e
G N M <gorakhnath1992@gmail.com>
2019-04-23 11:48:06 +0530

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -4,4 +4,9 @@
 	$dbUser = "root";
 	$dbPasswrod = "";
 	$mysqli = new mysqli($dbHost, $dbUser, $dbPasswrod, $dbDatabase);
+
+
+array('(service_centres.is_wh = 1  OR service_centres.is_wh = 1 )' => NULL)
+	
+	CI or uses
 ?>
